cc4987f475531a5ae6061e37329dd8ebbb2914da
peth <oopeth139oo@gmail.com>
2023-04-27 18:27:43 +0700

    change to grid

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const sketch = document.querySelector(".sketch");
-while(sketch.children.length != 50){
+const sketch = document.querySelector(".sketch-container");
+while(sketch.children.length != 256){
     let div = document.createElement("div");
     // div.innerText = "hi";
-    div.classList.add("grid");
+    div.classList.add("grid-item");
     sketch.appendChild(div);
 }
 console.log(sketch.children.length);
