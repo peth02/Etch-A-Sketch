f51062d4d8bdef624dc9d1bebbe01f1096bf9a27
peth <oopeth139oo@gmail.com>
2023-04-28 22:28:50 +0700

    Add clear button event

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const sketch = document.querySelector(".sketch-container");
-while(sketch.children.length != 256){
+while(sketch.children.length != 16*16){
     let div = document.createElement("div");
     // div.innerText = "hi";
     div.classList.add("grid-item");
@@ -13,5 +13,12 @@ const gridItem = [...document.querySelectorAll(".grid-item")];
 gridItem.forEach(item => {
     item.addEventListener("mouseover", () => {
         item.classList.add("color");
+    });
+});
+
+const clear = document.querySelector("#clear");
+clear.addEventListener("click", () => {
+    gridItem.forEach(item => {
+        item.classList.remove("color");
     });
 });
